ad07517dbedbb5dfb210f77ce5515a32140358f7
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2024-01-31 13:49:42 +0000

    Draggable map
    Map is draggable and works yey

--- javascript/map.js
+++ javascript/map.js
@@ -1,31 +1,20 @@
-var isDown = false;
-var startX, startY, startBackgroundX, startBackgroundY;
 
-var map = document.querySelector('.map');
-var mapRect = map.getBoundingClientRect();
-var imageWidth = 734; // your image's actual width
-var imageHeight = 573;
-map.addEventListener('mousedown', function (e) {
-    isDown = true;
-    startX = e.clientX;
-    startY = e.clientY;
-    startBackgroundX = parseInt(getComputedStyle(map).backgroundPositionX, 10);
-    startBackgroundY = parseInt(getComputedStyle(map).backgroundPositionY, 10);
-});
+gsap.registerPlugin(Draggable);
 
-map.addEventListener('mousemove', function (e) {
-    if (!isDown) return;
+var container = document.getElementById("container");
 
-    var x = e.clientX;
-    var y = e.clientY;
+function update() {
+    console.log("hi i am called");
+    Draggable.create(".map", {
+        bounds: container,
+        edgeResistance: 0.65,
+        type: "x,y",
+        throwProps: true,
+        autoScroll: true,
+    });
+}
+console.log("HI");
 
-    var newBackgroundX = startBackgroundX + x - startX;
-    var newBackgroundY = startBackgroundY + y - startY;
-    
-    map.style.backgroundPositionX = newBackgroundX + 'px';
-    map.style.backgroundPositionY = newBackgroundY + 'px';
-});
-
-map.addEventListener('mouseup', function () {
-    isDown = false;
+document.addEventListener("DOMContentLoaded", function () {
+    update();
 });
