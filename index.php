2aaff7fe1f6d8032bf8ce3419280925834d9088c
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2024-02-01 17:59:39 +0000

    Report page

--- index.php
+++ index.php
@@ -21,7 +21,9 @@ $f3->set('UI','ui/');		// folder for View templates
 //==============================================================================
 // Simple Example URL application routings
 
-//home page (index.html) -- actually just shows form entry page with a different title
+//==============================================================================
+// Home Page
+//==============================================================================
 $f3->route('GET /',
     function ($f3)
     {
@@ -32,6 +34,7 @@ $f3->route('GET /',
 );
 //==============================================================================
 // Log In Route
+//==============================================================================
 $f3->route('GET /login',
     function($f3)
     {
@@ -54,6 +57,7 @@ $f3->route('POST /login',
         {
             if($user->username == $username && $user->password == $password)
             {
+                LoadThirdplacesData($f3);
                 $f3->set('html_title','Simple Input Form');
                 $f3->set('content','map.html');
                 echo template::instance()->render('layout.html');
@@ -64,8 +68,8 @@ $f3->route('POST /login',
     }
 );
 //==============================================================================
+// Sign Up Route
 //==============================================================================
-//Sign up route
 $f3->route('POST /signup',
     function($f3)
     {
@@ -75,6 +79,13 @@ $f3->route('POST /signup',
 
         $users = new SimpleController('users');
         $userData = $users->GetData();
+
+        if(!preg_match('/^[a-zA-Z0-9._%+-]+@ed\.ac\.uk$/', $signupdata["username"] ))
+        {
+            echo 'Use a valid Edinburgh e-mail address';
+            return;
+        }
+
 
         foreach($userData as $user)
         {
@@ -87,25 +98,15 @@ $f3->route('POST /signup',
 
 
         $users->setNewUser($signupdata);
-
+        LoadThirdplacesData($f3);
         $f3->set('html_title','Simple Input Form');
         $f3->set('content','map.html');
         echo template::instance()->render('layout.html');
     }
 );
-// When using GET, provide a form for the user to upload an image via the file input type
-$f3->route('GET /simpleform',
-    function($f3)
-    {
-        $f3->set('html_title','Simple Input Form');
-        $f3->set('content','login.html');
-        echo template::instance()->render('layout.html');
-    }
-);
 //==============================================================================
-// When using POST (e.g.  form is submitted), invoke the controller, which will process
-// any data then return info we want to display. We display
-// the info here via the response.html template
+// To Delete Probably
+//==============================================================================
 $f3->route('POST /simpleform',
     function($f3)
     {
@@ -118,63 +119,43 @@ $f3->route('POST /simpleform',
 
         $f3->set('formData',$formdata);		// set info in F3 variable for access in response template
         $f3->set('html_title','Simple Example Response');
-        $f3->set('content','response.html');
+        $f3->set('content','report.html');
         echo template::instance()->render('layout.html');
     }
 );
 //==============================================================================
-$f3->route('GET /dataView',
-    function($f3)
-    {
-        $controller = new SimpleController('simpleModel');
-        $alldata = $controller->getData();
-
-        $f3->set("dbData", $alldata);
-        $f3->set('html_title','Viewing the data');
-        $f3->set('content','dataView.html');
-        echo template::instance()->render('layout.html');
-    }
-);
+// Load the data of the third places to display it
 //==============================================================================
-$f3->route('GET /editView',				// exactly the same as dataView, apart from the template used
-    function($f3)
-    {
-        $controller = new SimpleController('simpleModel');
-        $alldata = $controller->getData();
-
-        $f3->set("dbData", $alldata);
-        $f3->set('html_title','Viewing the data');
-        $f3->set('content','editView.html');
-        echo template::instance()->render('layout.html');
-    }
-);
-//==============================================================================
-$f3->route('POST /editView',
-    function($f3)
-    {
-        $controller = new SimpleController('simpleModel');
-        $controller->deleteFromDatabase($f3->get('POST.toDelete'));		// in this case, delete selected data record
-        $f3->reroute();
-    }
-);
-
-//==============================================================================
-
-$f3->route('GET /map',
-    function($f3)
-    {
+function LoadThirdplacesData($f3){
         $thirdplaces = new SimpleController('thirdplaces');
         $thirdplacesData = $thirdplaces->getData();
 
         $f3->set('thirdplacesData', $thirdplacesData);
 
+}
+//==============================================================================
+// Map
+//==============================================================================
+$f3->route('GET /map',
+    function($f3)
+    {
+        LoadThirdplacesData($f3);
         $f3->set('html_title', "FFF-SimpleExample");
         $f3->set('content','map.html');
         echo template::instance()->render('layout.html');;
     }
 );
-
-
+//==============================================================================
+// Report
+//==============================================================================
+$f3->route('GET /report',
+    function($f3)
+    {
+        $f3->set('html_title',"Report");
+        $f3->set('content','report.html');
+        echo template::instance()->render('layout.html');
+    }
+);
 //==============================================================================
 function pprint_var($var)
 {
