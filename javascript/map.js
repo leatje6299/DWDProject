691432e5e2de94c7e9efc724b73ca1ae9f2f304b
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2024-02-01 12:10:02 +0000

    Modal map

--- javascript/map.js
+++ javascript/map.js
@@ -4,7 +4,6 @@ gsap.registerPlugin(Draggable);
 var container = document.getElementById("container");
 
 function update() {
-    console.log("hi i am called");
     Draggable.create(".map", {
         bounds: container,
         edgeResistance: 0.1,
@@ -15,8 +14,16 @@ function update() {
         zIndex: -1
     });
 }
-console.log("HI");
 
+function openModal(name) {
+    console.log("clicked on" + name);
+    document.getElementById('modal-content').textContent = name;
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
 document.addEventListener("DOMContentLoaded", function () {
     update();
 });
